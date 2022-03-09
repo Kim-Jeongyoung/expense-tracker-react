6912dcb4c8487e3ff1692efcaff683b361be5330
Kim-Jeongyoung <jeongyoung2@gmail.com>
2022-03-09 06:56:25 -0800

    Edit ts at ExpenseItem props

--- src/components/Expenses/ExpenseItem.ts
+++ src/components/Expenses/ExpenseItem.ts
@@ -2,11 +2,7 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props: {
-  date: Date;
-  title: SVGStringList;
-  amount: number;
-}) {
+function ExpenseItem(props: { date: Date; title: string; amount: number }) {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
